897b044ba21b3fdd086c6170d06e7a2d3d99a98f
Kannan Vijayan <kannan@vijayan.ca>
2013-08-01 01:27:27 -0400

    Added object implementation.

---
+++ src/whisper/vm/object.cpp
@@ -0,0 +1,106 @@
+
+#include "vm/object.hpp"
+#include "value_inlines.hpp"
+#include "vm/heap_thing_inlines.hpp"
+#include "vm/shape_tree_inlines.hpp"
+#include <algorithm>
+
+namespace Whisper {
+namespace VM {
+
+
+Object::Object(Shape *shape, Object *prototype)
+  : ShapedHeapThing<HeapType::Object>(shape)
+{
+    prototype_ = prototype;
+    dynamicSlots_ = nullptr;
+}
+
+Object *
+Object::prototype() const
+{
+    return prototype_;
+}
+
+bool
+Object::hasDynamicSlots() const
+{
+    return dynamicSlots_.hasHeapThing();
+}
+
+Tuple *
+Object::dynamicSlots() const
+{
+    return dynamicSlots_;
+}
+
+uint32_t
+Object::numFixedSlots() const
+{
+    return this->objectValueCount() - FixedSlotsStart;
+}
+
+uint32_t
+Object::numDynamicSlots() const
+{
+    // This needs to search the shape lineage for the highest
+    // numbered, writable, dynamic value slot.
+    Shape *shape = shape_;
+    WH_ASSERT(shape);
+
+    uint32_t maxSlotIndex = 0;
+    bool found = false;
+    do {
+        if (!shape->hasValue() || !shape->isWritable())
+            continue;
+
+        ValueShape *valueShape = shape->toValueShape();
+        if (!valueShape->isDynamicSlot())
+            continue;
+
+        found = true;
+        maxSlotIndex = std::max(maxSlotIndex, valueShape->slotIndex());
+
+        shape = shape->maybeParent();
+
+    } while (shape);
+
+    if (!found)
+        return 0;
+
+    return maxSlotIndex + 1;
+}
+
+uint32_t
+Object::numSlots() const
+{
+    return numFixedSlots() + numDynamicSlots();
+}
+
+const Value &
+Object::fixedSlotValue(uint32_t idx) const
+{
+    WH_ASSERT(idx < numFixedSlots());
+    return valueRef(idx + FixedSlotsStart);
+}
+
+const Value &
+Object::dynamicSlotValue(uint32_t idx) const
+{
+    WH_ASSERT(idx < numDynamicSlots());
+    return dynamicSlots_->element(idx);
+}
+
+const Value &
+Object::slotValue(uint32_t idx) const
+{
+    WH_ASSERT(idx < numSlots());
+    if (idx < numFixedSlots())
+        return fixedSlotValue(idx);
+
+    return dynamicSlotValue(idx - numFixedSlots());
+}
+
+
+} // namespace VM
+} // namespace Whisper
---
+++ src/whisper/vm/object.hpp
@@ -0,0 +1,69 @@
+#ifndef WHISPER__VM__OBJECT_HPP
+#define WHISPER__VM__OBJECT_HPP
+
+#include "common.hpp"
+#include "debug.hpp"
+#include "value.hpp"
+#include "rooting.hpp"
+#include "vm/heap_thing.hpp"
+#include "vm/shape_tree.hpp"
+#include "vm/tuple.hpp"
+
+namespace Whisper {
+namespace VM {
+
+//
+// A regular javascript Object.
+//
+//      +---------------+
+//      | header        |
+//      +---------------+
+//      | shape         |
+//      | prototype     |
+//      | dynamicSlots  |
+//      | slotValue0    |
+//      | ...           |
+//      | slotValueN    |
+//      +---------------+
+//
+// The header flags store the following info:
+//      PreventExtensions
+//
+class Object : public ShapedHeapThing<HeapType::Object>
+{
+  public:
+    enum Flags : uint8_t {
+        PreventExtensions = 0x1
+    };
+
+    // Shape, Prototype, and DynamicSlots - 3 internal slots.
+    static constexpr unsigned FixedSlotsStart = 3;
+    
+  private:
+    NullableHeapThingValue<Object> prototype_;
+    NullableHeapThingValue<Tuple> dynamicSlots_;
+
+  public:
+    Object(Shape *shape, Object *prototype);
+
+    Object *prototype() const;
+
+    bool hasDynamicSlots() const;
+    Tuple *dynamicSlots() const;
+
+    uint32_t numFixedSlots() const;
+    uint32_t numDynamicSlots() const;
+    uint32_t numSlots() const;
+
+    const Value &fixedSlotValue(uint32_t idx) const;
+    const Value &dynamicSlotValue(uint32_t idx) const;
+    const Value &slotValue(uint32_t idx) const;
+};
+
+typedef HeapThingWrapper<Object> WrappedObject;
+
+
+} // namespace VM
+} // namespace Whisper
+
+#endif // WHISPER__VM__OBJECT_HPP
