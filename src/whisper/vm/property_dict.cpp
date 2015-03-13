000785bf82f306906d82c81d3703e0170bcbfe76
Kannan Vijayan <kannan@vijayan.ca>
2015-03-13 00:51:29 -0400

    Added PropertyDict cpp file.

---
+++ src/whisper/vm/property_dict.cpp
@@ -0,0 +1,17 @@
+
+#include "runtime_inlines.hpp"
+#include "vm/property_dict.hpp"
+
+namespace Whisper {
+namespace VM {
+
+
+/* static */ PropertyDict *
+PropertyDict::Create(AllocationContext acx, uint32_t capacity)
+{
+    return acx.createSized<PropertyDict>(CalculateSize(capacity), capacity);
+}
+
+
+} // namespace VM
+} // namespace Whisper
