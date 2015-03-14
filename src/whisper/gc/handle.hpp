15fb83cb2be2dd6811b4614d83f227bfb8aedbea
Kannan Vijayan <kannan@vijayan.ca>
2015-03-14 06:39:43 -0400

    Added MutArrayHandle and ArrayHandle.

--- src/whisper/gc/handle.hpp
+++ src/whisper/gc/handle.hpp
@@ -9,15 +9,12 @@ namespace Whisper {
 
 
 //
-// MutHandles are lightweight pointers to Local<> instances.  They
+// MutHandles are lightweight pointers to rooted values.  They
 // allow for mutation of the stored value (i.e. assignment).
 //
-
 template <typename T>
 class MutHandle
 {
-    static_assert(StackTraits<T>::Specialized,
-                  "StackTraits<T> not specialized.");
     typedef typename DerefTraits<T>::Type DerefType;
 
   private:
@@ -48,7 +45,7 @@ class MutHandle
         *valAddr_ = other;
     }
     inline void set(T &&other) {
-        *valAddr_ = other;
+        *valAddr_ = std::move(other);
     }
 
     inline T *address() const {
@@ -75,6 +72,62 @@ class MutHandle
         *valAddr_ = other;
         return other;
     }
+    const T &operator =(T &&other) {
+        WH_ASSERT(valAddr_ != nullptr);
+        *valAddr_ = std::move(other);
+        return other;
+    }
+};
+
+template <typename T>
+class MutArrayHandle
+{
+    typedef typename DerefTraits<T>::Type DerefType;
+
+  private:
+    T * const valAddr_;
+    uint32_t length_;
+
+    inline MutArrayHandle(T *valAddr, uint32_t length)
+      : valAddr_(valAddr),
+        length_(length)
+    {}
+
+  public:
+    inline MutArrayHandle(Local<T> &stackVal)
+      : MutArrayHandle(stackVal.address(), 1)
+    {}
+
+    inline static MutArrayHandle<T> FromTrackedPointer(T *valAddr,
+                                                       uint32_t length)
+    {
+        return MutArrayHandle<T>(valAddr, length);
+    }
+
+    inline const T &get(uint32_t i) const {
+        WH_ASSERT(i < length_);
+        return valAddr_[i];
+    }
+    inline T &get(uint32_t i) {
+        WH_ASSERT(i < length_);
+        return valAddr_[i];
+    }
+
+    inline void set(uint32_t i, const T &other) {
+        WH_ASSERT(i < length_);
+        valAddr_[i] = other;
+    }
+    inline void set(uint32_t i, T &&other) {
+        WH_ASSERT(i < length_);
+        valAddr_[i] = other;
+    }
+
+    inline const T &operator [](uint32_t idx) const {
+        return get(idx);
+    }
+    inline T &operator [](uint32_t idx) {
+        return get(idx);
+    }
 };
 
 
@@ -82,13 +135,9 @@ class MutHandle
 // Handles are const pointers to values rooted on the stack with
 // Local.
 //
-
 template <typename T>
 class Handle
 {
-    static_assert(StackTraits<T>::Specialized,
-                  "StackTraits<T> not specialized.");
-
     typedef typename DerefTraits<T>::Type DerefType;
 
   private:
@@ -133,6 +182,52 @@ class Handle
     }
 
     const Handle<T> &operator =(const Handle<T> &other) = delete;
+};
+
+template <typename T>
+class ArrayHandle
+{
+    typedef typename DerefTraits<T>::Type DerefType;
+
+  private:
+    T * const valAddr_;
+    uint32_t length_;
+
+    inline ArrayHandle(T *valAddr, uint32_t length)
+      : valAddr_(valAddr),
+        length_(length)
+    {}
+
+  public:
+    inline ArrayHandle(Local<T> &stackVal)
+      : ArrayHandle(stackVal.address(), 1)
+    {}
+
+    inline ArrayHandle(const MutArrayHandle<T> &mutHandle)
+      : ArrayHandle(mutHandle.valAddr_, mutHandle.length)
+    {}
+
+    inline static ArrayHandle<T> FromTrackedPointer(T *valAddr,
+                                                    uint32_t length)
+    {
+        return ArrayHandle<T>(valAddr, length);
+    }
+
+    inline const T &get(uint32_t i) const {
+        WH_ASSERT(i < length_);
+        return valAddr_[i];
+    }
+    inline T get(uint32_t i) {
+        WH_ASSERT(i < length_);
+        return valAddr_[i];
+    }
+
+    inline const T &operator [](uint32_t idx) const {
+        return get(idx);
+    }
+    inline T operator [](uint32_t idx) {
+        return get(idx);
+    }
 };
 
 
