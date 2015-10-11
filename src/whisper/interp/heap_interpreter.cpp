cae657a32487a7458be46ff245abfbd73bce7323
Kannan Vijayan <kannan@vijayan.ca>
2015-10-11 00:08:53 -0400

    Started name resolution code in heap interpreter implementation.

--- src/whisper/interp/heap_interpreter.cpp
+++ src/whisper/interp/heap_interpreter.cpp
@@ -1,6 +1,7 @@
 
 #include "parser/syntax_tree.hpp"
 #include "parser/packed_syntax.hpp"
+#include "vm/function.hpp"
 #include "vm/runtime_state.hpp"
 #include "interp/heap_interpreter.hpp"
 
@@ -58,11 +59,18 @@ HeapInterpretLoop(ThreadContext* cx)
 }
 
 
-Result<VM::Frame*>
-CreateInitialSyntaxFrame(ThreadContext* cx,
-                         Handle<VM::EntryFrame*> entryFrame)
+static OkResult
+ResolveSyntaxNameLookup(ThreadContext* cx,
+                             Handle<VM::SyntaxFrame*> frame,
+                             VM::ControlFlow const& flow)
 {
-    Local<VM::SyntaxTreeFragment*> stFrag(cx, entryFrame->stFrag());
+    return ErrorVal();
+}
+
+static OkResult
+StepSyntaxNameLookup(ThreadContext* cx, Handle<VM::SyntaxFrame*> frame)
+{
+    Local<VM::SyntaxTreeFragment*> stFrag(cx, frame->stFrag());
 
     // Get the name of the syntax handler method.
     Local<VM::String*> name(cx, cx->runtimeState()->syntaxHandlerName(stFrag));
@@ -72,10 +80,133 @@ CreateInitialSyntaxFrame(ThreadContext* cx,
         return ErrorVal();
     }
 
-    // The lookup for the syntax handler method may invoke computation,
-    // and thus i
+    // Look up the property on the scope object.
+    Local<VM::ScopeObject*> scope(cx, frame->entryFrame()->scope());
+    PropertyLookupResult lookupResult = GetObjectProperty(cx,
+        scope.handle().convertTo<VM::Wobject*>(), name);
 
+    if (lookupResult.isError()) {
+        return ResolveSyntaxNameLookup(cx, frame,
+                VM::ControlFlow::Error());
+    }
+
+    if (lookupResult.isNotFound()) {
+        cx->setExceptionRaised("Lookup name not found", name.get());
+        return ResolveSyntaxNameLookup(cx, frame,
+            VM::ControlFlow::Exception());
+    }
+
+    if (lookupResult.isFound()) {
+        Local<VM::PropertyDescriptor> descriptor(cx, lookupResult.descriptor());
+        Local<VM::LookupState*> lookupState(cx, lookupResult.lookupState());
+
+        // Handle a value binding by returning the value.
+        if (descriptor->isValue())
+            return ResolveSyntaxNameLookup(cx, frame,
+                    VM::ControlFlow::Value(descriptor->valBox()));
+
+        // Handle a method binding by creating a bound FunctionObject
+        // from the method.
+        if (descriptor->isMethod()) {
+            // Create a new function object bound to the scope.
+            Local<VM::ValBox> scopeVal(cx, VM::ValBox::Object(scope.get()));
+            Local<VM::Function*> func(cx, descriptor->method());
+            Local<VM::FunctionObject*> funcObj(cx);
+            if (!funcObj.setResult(VM::FunctionObject::Create(
+                    cx->inHatchery(), func, scopeVal, lookupState)))
+            {
+                return ErrorVal();
+            }
+
+            return ResolveSyntaxNameLookup(cx, frame,
+                    VM::ControlFlow::Value(VM::ValBox::Object(funcObj.get())));
+        }
+
+        WH_UNREACHABLE("PropertyDescriptor not one of Value, Method.");
+        return ErrorVal();
+    }
+
+    WH_UNREACHABLE("Property lookup not one of Found, NotFound, Error.");
     return ErrorVal();
+}
+
+
+Result<VM::SyntaxFrame*>
+CreateInitialSyntaxFrame(ThreadContext* cx,
+                         Handle<VM::EntryFrame*> entryFrame)
+{
+    Local<VM::SyntaxTreeFragment*> stFrag(cx, entryFrame->stFrag());
+
+    Local<VM::SyntaxFrame*> stFrame(cx);
+    if (!stFrame.setResult(VM::SyntaxFrame::Create(cx->inHatchery(),
+            entryFrame, stFrag,
+            ResolveSyntaxNameLookup,
+            StepSyntaxNameLookup)))
+    {
+        return ErrorVal();
+    }
+
+    return OkVal(stFrame.get());
+}
+
+
+static PropertyLookupResult
+GetPropertyHelper(ThreadContext* cx,
+                  Handle<VM::ValBox> receiver,
+                  Handle<VM::Wobject*> object,
+                  Handle<VM::String*> name)
+{
+    Local<VM::LookupState*> lookupState(cx);
+    Local<VM::PropertyDescriptor> propDesc(cx);
+
+    Result<bool> lookupResult = VM::Wobject::LookupProperty(
+        cx->inHatchery(), object, name, &lookupState, &propDesc);
+    if (!lookupResult)
+        return PropertyLookupResult::Error();
+
+    // If binding not found, return void control flow.
+    if (!lookupResult.value())
+        return PropertyLookupResult::NotFound();
+
+    // Found binding.
+    WH_ASSERT(propDesc->isValid());
+    return PropertyLookupResult::Found(lookupState.get(), propDesc.get());
+}
+
+PropertyLookupResult
+GetValueProperty(ThreadContext* cx,
+                 Handle<VM::ValBox> value,
+                 Handle<VM::String*> name)
+{
+    // Check if the value is an object.
+    if (value->isPointer()) {
+        Local<VM::Wobject*> object(cx, value->objectPointer());
+        return GetPropertyHelper(cx, value, object, name);
+    }
+
+    // Check if the value is a fixed integer.
+    if (value->isInteger()) {
+        Local<VM::Wobject*> immInt(cx, cx->threadState()->immIntDelegate());
+        return GetPropertyHelper(cx, value, immInt, name);
+    }
+
+    // Check if the value is a fixed boolean.
+    if (value->isBoolean()) {
+        Local<VM::Wobject*> immBool(cx, cx->threadState()->immBoolDelegate());
+        return GetPropertyHelper(cx, value, immBool, name);
+    }
+
+    return cx->setInternalError("Cannot look up property on a given "
+                                "primitive value");
+}
+
+PropertyLookupResult
+GetObjectProperty(ThreadContext* cx,
+                  Handle<VM::Wobject*> object,
+                  Handle<VM::String*> name)
+{
+    Local<VM::ValBox> val(cx, VM::ValBox::Object(object));
+    return GetPropertyHelper(cx, val, object, name);
 }
 
 
--- src/whisper/interp/heap_interpreter.hpp
+++ src/whisper/interp/heap_interpreter.hpp
@@ -6,6 +6,8 @@
 #include "vm/predeclare.hpp"
 #include "vm/source_file.hpp"
 #include "vm/scope_object.hpp"
+#include "vm/properties.hpp"
+#include "vm/lookup_state.hpp"
 #include "vm/control_flow.hpp"
 #include "vm/frame.hpp"
 
@@ -19,8 +21,86 @@ VM::ControlFlow HeapInterpretSourceFile(ThreadContext* cx,
 
 VM::ControlFlow HeapInterpretLoop(ThreadContext* cx);
 
-Result<VM::Frame*> CreateInitialSyntaxFrame(ThreadContext* cx,
-                                            Handle<VM::EntryFrame*> entryFrame);
+Result<VM::SyntaxFrame*> CreateInitialSyntaxFrame(
+        ThreadContext* cx, Handle<VM::EntryFrame*> entryFrame);
+
+
+// Property lookup helpers.
+class PropertyLookupResult
+{
+  public:
+    enum class Outcome {
+        Error,
+        NotFound,
+        Found
+    };
+
+  private:
+    Outcome outcome_;
+    StackField<VM::LookupState*> lookupState_;
+    StackField<VM::PropertyDescriptor> descriptor_;
+
+    PropertyLookupResult(Outcome outcome,
+                         VM::LookupState* lookupState,
+                         VM::PropertyDescriptor const& descriptor)
+      : outcome_(outcome),
+        lookupState_(lookupState),
+        descriptor_(descriptor)
+    {}
+
+  public:
+    PropertyLookupResult(ErrorT_ const& error)
+      : PropertyLookupResult(Outcome::Error, nullptr,
+                             VM::PropertyDescriptor())
+    {}
+
+    static PropertyLookupResult Error() {
+        return PropertyLookupResult(Outcome::Error, nullptr,
+                                    VM::PropertyDescriptor());
+    }
+    static PropertyLookupResult NotFound() {
+        return PropertyLookupResult(Outcome::NotFound, nullptr,
+                                    VM::PropertyDescriptor());
+    }
+    static PropertyLookupResult Found(
+            VM::LookupState* lookupState,
+            VM::PropertyDescriptor const& descriptor)
+    {
+        WH_ASSERT(lookupState != nullptr);
+        WH_ASSERT(descriptor.isValid());
+        return PropertyLookupResult(Outcome::Found, lookupState, descriptor);
+    }
+
+    Outcome outcome() const {
+        return outcome_;
+    }
+    bool isError() const {
+        return outcome() == Outcome::Error;
+    }
+    bool isNotFound() const {
+        return outcome() == Outcome::NotFound;
+    }
+    bool isFound() const {
+        return outcome() == Outcome::Found;
+    }
+
+    VM::LookupState *lookupState() const {
+        WH_ASSERT(isFound());
+        return lookupState_;
+    }
+    VM::PropertyDescriptor const& descriptor() const {
+        WH_ASSERT(isFound());
+        return descriptor_;
+    }
+};
+
+PropertyLookupResult GetValueProperty(ThreadContext* cx,
+                                      Handle<VM::ValBox> value,
+                                      Handle<VM::String*> name);
+
+PropertyLookupResult GetObjectProperty(ThreadContext* cx,
+                                       Handle<VM::Wobject*> object,
+                                       Handle<VM::String*> name);
 
 
 } // namespace Interp
