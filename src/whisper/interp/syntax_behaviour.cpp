8bd0b5556df1485583b9e15167c826e1ab99ed51
Kannan Vijayan <kannan@vijayan.ca>
2016-12-23 00:33:10 -0500

    Added const statement evaluation.

--- src/whisper/interp/syntax_behaviour.cpp
+++ src/whisper/interp/syntax_behaviour.cpp
@@ -34,9 +34,7 @@ DECLARE_SYNTAX_FN_(ExprStmt)
 DECLARE_SYNTAX_FN_(ReturnStmt)
 */
 DECLARE_SYNTAX_FN_(DefStmt)
-/*
 DECLARE_SYNTAX_FN_(ConstStmt)
-*/
 DECLARE_SYNTAX_FN_(VarStmt)
 
 DECLARE_SYNTAX_FN_(CallExpr)
@@ -105,9 +103,7 @@ BindSyntaxHandlers(AllocationContext acx, VM::GlobalScope* scope)
     BIND_GLOBAL_METHOD_(ReturnStmt);
 */
     BIND_GLOBAL_METHOD_(DefStmt);
-/*
     BIND_GLOBAL_METHOD_(ConstStmt);
-*/
     BIND_GLOBAL_METHOD_(VarStmt);
 
     BIND_GLOBAL_METHOD_(CallExpr);
@@ -327,6 +323,44 @@ IMPL_SYNTAX_FN_(DefStmt)
 
     // Def statements always yield void.
     return VM::CallResult::Void();
+}
+
+IMPL_SYNTAX_FN_(ConstStmt)
+{
+    if (args.length() != 1) {
+        Local<VM::Exception*> exc(cx);
+        if (!exc.setResult(VM::InternalException::Create(cx->inHatchery(),
+                       "@ConstStmt called with wrong number of arguments.")))
+        {
+            return ErrorVal();
+        }
+        return VM::CallResult::Exc(callInfo->frame(), exc);
+    }
+
+    WH_ASSERT(args.get(0)->isNode());
+    WH_ASSERT(args.get(0)->toNode()->nodeType() == AST::ConstStmt);
+
+    Local<VM::SyntaxNodeRef> stRef(cx, args.get(0)->toNode());
+    Local<VM::PackedSyntaxTree*> pst(cx, stRef->pst());
+    Local<AST::PackedConstStmtNode> constStmtNode(cx, stRef->astConstStmt());
+
+    WH_ASSERT(constStmtNode->numBindings() > 0);
+
+    SpewInterpNote("Syntax_ConstStmt: Interpreting");
+
+    // Create a ConstSyntaxFrame for it.
+    Local<VM::Frame*> frame(cx, callInfo->frame());
+    Local<VM::EntryFrame*> entryFrame(cx, frame->ancestorEntryFrame());
+    Local<VM::SyntaxTreeFragment*> stFrag(cx, args.get(0));
+    Local<VM::VarSyntaxFrame*> syntaxFrame(cx);
+    if (!syntaxFrame.setResult(VM::VarSyntaxFrame::Create(
+            cx->inHatchery(), frame, entryFrame, stFrag, 0)))
+    {
+        return ErrorVal();
+    }
+
+    // Def statements always yield void.
+    return VM::CallResult::Continue(syntaxFrame.get());
 }
 
 IMPL_SYNTAX_FN_(VarStmt)
